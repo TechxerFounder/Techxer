93f28c579bbf0d27b5d6f13caf4ebe764aff26a4
TechxerFounder <79297101+TechxerFounder@users.noreply.github.com>
2021-03-20 16:28:19 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6055c5b2f029cd001f5e1d9f",
+				"6055c5b2f029cd001f5e1da1"
+			]
+		},
+		"6055c5b2f029cd001f5e1d9f": {
+			"id": "6055c5b2f029cd001f5e1d9f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6055c5b2f029cd001f5e1da1": {
+			"id": "6055c5b2f029cd001f5e1da1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
